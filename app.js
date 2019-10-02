cb97cba10bcdaef44765be13f6b05135fcf03338
Tevin Sharp <sharp@Tevins-MacBook-Air.local>
2019-10-02 18:36:52 -0500

    removed || 3000 which listens locally

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.get("/contact-me", function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT, function(){
   console.log("This server is now live!");
 });
