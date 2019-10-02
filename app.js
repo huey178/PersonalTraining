7e106cbe24bc7c382e202a1440361261b1d6176e
Tevin Sharp <sharp@Tevins-MacBook-Air.local>
2019-10-01 19:17:16 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+//jshint esversion:6
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+  res.render('home');
+});
+
+app.get("/meet-tyler", function (req,res){
+  res.render("meet-tyler");
+});
+
+
+app.get("/contact-me", function(req,res){
+  res.render("contact-me");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("This server is now live!");
+});
